143ef592c1434f8a69c33ec8ae0ed676b47a7db4
sunnygupta <sunnygupta1890@gmail.com>
2015-06-28 20:02:47 +0530

    Added final to request and response

--- src/main/java/com/tryout/TimePassServlet.java
+++ src/main/java/com/tryout/TimePassServlet.java
@@ -9,12 +9,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class TimePassServlet extends HttpServlet{
+public class TimePassServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
 
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
         System.out.println("submitted here");
         List<String> list = new ArrayList<String>();
         list.add("kamal");
